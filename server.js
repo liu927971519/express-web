121f87ee98c76d1c7e7be0707b0d82046b3fe046
liulijian <927971519@qq.com>
2018-12-19 18:54:08 +0800

    添加对缓存控制的支持

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require('express'); // 服务
 const compression = require('compression'); // gzip
 const path = require('path');
 const app = express();
+
 app.use(compression());
-app.use(express.static(__dirname + '/dist'));
+
+app.use(express.static(__dirname + '/dist', {
+  setHeaders: setCustomCacheControl
+}));
+
 app.get('*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
 app.listen(process.env.PORT || 3002, function() {
-	console.log(`已经启动服务： http://127.0.0.1:${process.env.PORT || 3002}`)
+  console.log(`已经启动服务： http://127.0.0.1:${process.env.PORT || 3002}`)
 });
+
+// 缓存控制
+function setCustomCacheControl(res, path) {
+  // 对html格式文件不设置缓存
+  if (express.static.mime.lookup(path) == 'text/html') {
+    res.setHeader('Cache-Control', 'no-cache');
+  } else {
+    // 其他静态资源使用强缓存
+    res.setHeader('Cache-Control', 'public, max-age=3600')
+  }
+}
